7e6b7d1b0c775014aed525a7b0600c5ab0b15a81
vande2025 <vanderlei.duarte@escola.pr.gov.br>
2025-07-30 16:16:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', function(){
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
